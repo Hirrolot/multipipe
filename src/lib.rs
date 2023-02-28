2b16bbbcb651d3aec041eac4478b5f7ec5c2ef53
Hirrolot <hirrolot@gmail.com>
2023-03-01 03:26:24 +0600

    Remove the `with` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 mod pipeline;
 mod pipeline_call;
-mod with;
 
 pub use pipeline::*;
 pub use pipeline_call::*;
-pub use with::*;
 
 pub trait Pipe: Sized {
     fn pipe<Out, Types, F>(self, f: impl Into<Pipeline<Types, F>>) -> Out
@@ -21,19 +19,6 @@ where
         Pipeline<Types, F>: PipelineCall<Self, Out>,
     {
         f.into().call(self)
-    }
-}
-
-pub trait PipelineFn<In, Out> {
-    fn call(self, x: In) -> Out;
-}
-
-impl<In, Out, F> PipelineFn<In, Out> for F
-where
-    F: FnOnce(In) -> Out,
-{
-    fn call(self, x: In) -> Out {
-        self(x)
     }
 }
 
@@ -78,43 +63,5 @@ mod tests {
 
         let x: E = A.pipe((a_to_b, b_to_c, c_to_d, d_to_e));
         assert_eq!(x, E);
-    }
-
-    #[test]
-    fn pipe_with() {
-        fn ab_to_c(_: A, _: B) -> C {
-            C
-        }
-        fn c_to_d(_: C) -> D {
-            D
-        }
-
-        let x: C = B.pipe(ab_to_c.with(A));
-        assert_eq!(x, C);
-
-        let x: D = B.pipe((ab_to_c.with(A), c_to_d));
-        assert_eq!(x, D);
-    }
-
-    #[test]
-    fn with() {
-        fn ab_to_c(_: A, _: B) -> C {
-            C
-        }
-        fn abc_to_d(_: A, _: B, _: C) -> D {
-            D
-        }
-        fn abcd_to_e(_: A, _: B, _: C, _: D) -> E {
-            E
-        }
-
-        let b_to_c = ab_to_c.with(A);
-        assert_eq!(b_to_c(B), C);
-
-        let c_to_d = abc_to_d.with(A, B);
-        assert_eq!(c_to_d(C), D);
-
-        let d_to_e = abcd_to_e.with(A, B, C);
-        assert_eq!(d_to_e(D), E);
     }
 }
--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,12 +1,10 @@
 use std::marker::PhantomData;
 
-use crate::PipelineFn;
-
 pub struct Pipeline<T, F>(pub(crate) F, PhantomData<T>);
 
 impl<T1, T2, F1> From<F1> for Pipeline<(T1, T2), F1>
 where
-    F1: PipelineFn<T1, T2>,
+    F1: FnOnce(T1) -> T2,
 {
     fn from(f: F1) -> Self {
         Self(f, PhantomData)
@@ -15,8 +13,8 @@ where
 
 impl<T1, T2, T3, F1, F2> From<(F1, F2)> for Pipeline<(T1, T2, T3), (F1, F2)>
 where
-    F1: PipelineFn<T1, T2>,
-    F2: PipelineFn<T2, T3>,
+    F1: FnOnce(T1) -> T2,
+    F2: FnOnce(T2) -> T3,
 {
     fn from((f1, f2): (F1, F2)) -> Self {
         Self((f1, f2), PhantomData)
@@ -25,9 +23,9 @@ where
 
 impl<T1, T2, T3, T4, F1, F2, F3> From<(F1, F2, F3)> for Pipeline<(T1, T2, T3, T4), (F1, F2, F3)>
 where
-    F1: PipelineFn<T1, T2>,
-    F2: PipelineFn<T2, T3>,
-    F3: PipelineFn<T3, T4>,
+    F1: FnOnce(T1) -> T2,
+    F2: FnOnce(T2) -> T3,
+    F3: FnOnce(T3) -> T4,
 {
     fn from((f1, f2, f3): (F1, F2, F3)) -> Self {
         Self((f1, f2, f3), PhantomData)
@@ -37,10 +35,10 @@ where
 impl<T1, T2, T3, T4, T5, F1, F2, F3, F4> From<(F1, F2, F3, F4)>
     for Pipeline<(T1, T2, T3, T4, T5), (F1, F2, F3, F4)>
 where
-    F1: PipelineFn<T1, T2>,
-    F2: PipelineFn<T2, T3>,
-    F3: PipelineFn<T3, T4>,
-    F4: PipelineFn<T4, T5>,
+    F1: FnOnce(T1) -> T2,
+    F2: FnOnce(T2) -> T3,
+    F3: FnOnce(T3) -> T4,
+    F4: FnOnce(T4) -> T5,
 {
     fn from((f1, f2, f3, f4): (F1, F2, F3, F4)) -> Self {
         Self((f1, f2, f3, f4), PhantomData)
