907b02c902a66a3797645121a968ba47217d277c
Hirrolot <hirrolot@gmail.com>
2023-03-06 22:35:48 +0600

    Add `Pipe::{pipe_ref, pipe_mut}`

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ pub trait Pipe: Sized {
     fn pipe<Out, Types, F>(self, f: impl Into<Pipeline<Types, F>>) -> Out
     where
         Pipeline<Types, F>: PipelineCall<Self, Out>;
+
+    fn pipe_ref<Out>(&self, f: impl FnOnce(&Self) -> Out) -> Out;
+
+    fn pipe_mut<Out>(&mut self, f: impl FnOnce(&mut Self) -> Out) -> Out;
 }
 
 impl<T> Pipe for T
@@ -19,6 +23,14 @@ where
         Pipeline<Types, F>: PipelineCall<Self, Out>,
     {
         f.into().call(self)
+    }
+
+    fn pipe_ref<Out>(&self, f: impl FnOnce(&Self) -> Out) -> Out {
+        f(self)
+    }
+
+    fn pipe_mut<Out>(&mut self, f: impl FnOnce(&mut Self) -> Out) -> Out {
+        f(self)
     }
 }
 
@@ -42,6 +54,12 @@ mod tests {
         fn a_to_b(_: A) -> B {
             B
         }
+        fn a_to_b_ref(_: &A) -> B {
+            B
+        }
+        fn a_to_b_mut(_: &mut A) -> B {
+            B
+        }
         fn b_to_c(_: B) -> C {
             C
         }
@@ -53,6 +71,12 @@ mod tests {
         }
 
         let x: B = A.pipe(a_to_b);
+        assert_eq!(x, B);
+
+        let x: B = A.pipe_ref(a_to_b_ref);
+        assert_eq!(x, B);
+
+        let x: B = A.pipe_mut(a_to_b_mut);
         assert_eq!(x, B);
 
         let x: C = A.pipe((a_to_b, b_to_c));
